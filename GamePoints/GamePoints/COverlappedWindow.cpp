5c99240bab41fe199dce5c407175a6787cc3d873
IvanShafran <vanobox07@mail.ru>
2016-04-13 17:49:54 +0300

    create empty window

---
+++ GamePoints/GamePoints/COverlappedWindow.cpp
@@ -0,0 +1,117 @@
+#include "COverlappedWindow.h"
+#include <Windows.h>
+#include <iostream>
+
+COverlappedWindow::COverlappedWindow()
+{
+
+}
+
+COverlappedWindow::~COverlappedWindow()
+{
+
+}
+
+LRESULT __stdcall COverlappedWindow::windowProc(HWND handle, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	if (message == WM_ERASEBKGND) {
+		return true;
+	}
+
+	if (message == WM_NCCREATE) {
+		COverlappedWindow* window = reinterpret_cast<COverlappedWindow*>(reinterpret_cast<CREATESTRUCT*>(lParam)->lpCreateParams);
+		::SetWindowLongPtr(handle, GWLP_USERDATA, reinterpret_cast<LONG>(window));
+	}
+	else {
+		COverlappedWindow* window = reinterpret_cast<COverlappedWindow*>(::GetWindowLongPtr(handle, GWLP_USERDATA));
+		switch (message) {
+		case WM_CREATE:
+			window->OnCreate(handle);
+			break;
+		case WM_SIZE:
+			window->OnSize(lParam);
+			break;
+		case WM_SETFOCUS:
+			window->OnSetFocus();
+			break;
+		case WM_COMMAND:
+			window->OnCommand(wParam, lParam);
+			break;
+		case WM_CLOSE:
+			if (!window->OnClose()) {
+				return 0;
+			}
+			break;
+		case WM_DESTROY:
+			window->OnDestroy();
+			break;
+		default:
+			break;
+		}
+	}
+
+	return DefWindowProc(handle, message, wParam, lParam);
+}
+
+bool COverlappedWindow::RegisterClass(HINSTANCE instance)
+{
+	WNDCLASSEX windowClass;
+
+	::ZeroMemory(&windowClass, sizeof(windowClass));
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = COverlappedWindow::windowProc;
+	windowClass.hIcon;
+	windowClass.hInstance = instance;
+	windowClass.lpszClassName = L"COverlappedWindow";
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.lpszMenuName = L"MainMenu";
+
+	return ::RegisterClassEx(&windowClass) != 0;
+}
+
+bool COverlappedWindow::Create(HINSTANCE instance)
+{
+	hInstance = instance;
+
+	COverlappedWindow::handle = ::CreateWindowEx(0,
+		L"COverlappedWindow",
+		L"GamePoints",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		0,
+		0,
+		instance,
+		this);
+
+	return COverlappedWindow::handle != 0;
+}
+
+void COverlappedWindow::OnCreate(HWND handle) {
+}
+
+void COverlappedWindow::OnSize(LPARAM lParam) {
+}
+
+void COverlappedWindow::OnSetFocus() {
+}
+
+bool COverlappedWindow::OnClose() {
+	return true;
+}
+
+void COverlappedWindow::OnCommand(WPARAM wParam, LPARAM lParam) {
+}
+
+void COverlappedWindow::OnDestroy()
+{
+	::PostQuitMessage(0);
+}
+
+void COverlappedWindow::Show(int cmdShow)
+{
+	::ShowWindow(handle, cmdShow);
+}
---
+++ GamePoints/GamePoints/COverlappedWindow.h
@@ -0,0 +1,26 @@
+#include <Windows.h>
+
+class COverlappedWindow {
+public:
+	COverlappedWindow();
+	~COverlappedWindow();
+
+	static bool RegisterClass(HINSTANCE instance);
+
+	bool Create(HINSTANCE instance);
+
+	void Show(int cmdShow);
+
+	HACCEL haccel;
+	HWND handle;
+protected:
+	void OnCreate(HWND handle);
+	void OnSize(LPARAM lParam);
+	void OnSetFocus();
+	void OnDestroy();
+	void OnCommand(WPARAM wParam, LPARAM lParam);
+	bool OnClose();
+private:
+	HINSTANCE hInstance;
+	static LRESULT __stdcall windowProc(HWND handle, UINT message, WPARAM wParam, LPARAM lParam);
+};
